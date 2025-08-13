b00b07825cdbe759d328dbbe31d80cb2039d2e60
roger.lee <lpc@iwhalecloud.com>
2025-08-13 15:54:12 +0800

    task<1>-重构core

--- config/SQL/domain-sample-pg.sql
+++ config/SQL/domain-sample-pg.sql
@@ -1,24 +1,21 @@
 DROP TABLE IF EXISTS family;
 CREATE TABLE family(
-                       ID NUMERIC(6) NOT NULL,
+                       ID BIGSERIAL PRIMARY KEY,
                        name VARCHAR(255),
-                       person_count NUMERIC(6),
-                       PRIMARY KEY (ID)
+                       person_count NUMERIC(6)
 );
 
 COMMENT ON TABLE family IS '用户表';
 COMMENT ON COLUMN family.ID IS '自增主键';
 COMMENT ON COLUMN family.name IS '名称';
 COMMENT ON COLUMN family.person_count IS '家庭成员数量';
-CREATE SEQUENCE IF NOT EXISTS seq_family_id AS BIGINT START WITH 1 INCREMENT BY 1;
 
 DROP TABLE IF EXISTS family_address;
 CREATE TABLE family_address(
-                               ID NUMERIC(6) NOT NULL,
-                               family_id NUMERIC(6),
+                               ID BIGSERIAL PRIMARY KEY,
+                               family_id BIGINT,
                                family_name VARCHAR(255),
-                               address_name VARCHAR(255),
-                               PRIMARY KEY (ID)
+                               address_name VARCHAR(255)
 );
 
 COMMENT ON TABLE family_address IS '用户家庭住址';
@@ -26,17 +23,15 @@ COMMENT ON COLUMN family_address.ID IS '自增主键';
 COMMENT ON COLUMN family_address.family_id IS '家庭ID';
 COMMENT ON COLUMN family_address.family_name IS '家庭名称';
 COMMENT ON COLUMN family_address.address_name IS '地址';
-CREATE SEQUENCE  IF NOT EXISTS seq_family_address_id AS BIGINT START WITH 1 INCREMENT BY 1;
 
 DROP TABLE IF EXISTS family_member;
 CREATE TABLE family_member(
-                              ID NUMERIC(6) NOT NULL,
-                              family_id NUMERIC(6),
+                              ID BIGSERIAL PRIMARY KEY,
+                              family_id BIGINT,
                               family_name VARCHAR(255),
                               name VARCHAR(255),
                               phone VARCHAR(255),
-                              type VARCHAR(255),
-                              PRIMARY KEY (ID)
+                              type VARCHAR(255)
 );
 
 COMMENT ON TABLE family_member IS '家庭成员';
@@ -46,5 +41,4 @@ COMMENT ON COLUMN family_member.family_name IS '家庭名称';
 COMMENT ON COLUMN family_member.name IS '姓名';
 COMMENT ON COLUMN family_member.phone IS '电话';
 COMMENT ON COLUMN family_member.type IS '成员关系';
-CREATE SEQUENCE IF NOT EXISTS seq_family_member_id AS BIGINT START WITH 1 INCREMENT BY 1;
 
